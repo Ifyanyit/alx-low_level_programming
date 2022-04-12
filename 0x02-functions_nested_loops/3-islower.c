5bbea4c3e8c7bf4224c362a09073af6e49d18934
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-04-12 16:57:45 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * This function checks character to check the case
+ * Return:0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+
+}
+
