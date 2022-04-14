be58f1eb3068442d4cf1ff97632213fded9cc3da
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-04-14 01:26:02 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+  * main - test function that prints if int is positive or negative
+  * Returns 0
+  */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	p0sitive_or_negative(i);
+	return 0;
+}
