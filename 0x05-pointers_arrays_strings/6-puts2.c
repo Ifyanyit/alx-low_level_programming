8ce0619feace0ee7d9bba177be1619368c4b0c8f
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-04-20 20:10:51 -0700

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *puts2 - prints every other char of string, starting with 1st char.
+  *@str: a pointer to an int
+  *Return - void which means answer is correct.
+  */
+
+void puts2(char *str)
+{
+	in a;
+        for ( a = 0; str[a] != '\0; a++)
+	{
+		if(a % 2 = 0);
+	{
+		_putchar(str[a]);
+	}
+
+	_putchar('\n');
+	}
+}
