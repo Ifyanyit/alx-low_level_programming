68fcb5518fbf5a8eeab0940fc4e0f6e5fdb6285e
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-04-20 19:06:01 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,15 +8,13 @@
 
 void _puts(char *str)
 {
-	char *c;
 	int m;
 
-	c = str;
-	for(m = 0; str[m];!= '\0'; m++)
+	for(m = 0; str[m]; != '\0'; m++)
 	{
 		_putchar(str[m]);
 	}
 
 	_putchar('\n');
-	return (0);
+	return;
 }
