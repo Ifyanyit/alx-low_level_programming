d6a8d53eba4cee504b47241e84e8eca10a2fbd21
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-04-20 18:03:46 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-  *_strlen - function that returns the lenght of a string.
-  *@s: A pointer to an int that changes.
-  *return: Void which means that our answer is correct.
-  */
+ *_strlen - function that returns the lenght of a string.
+ *@s: A pointer to an int that changed/ updated
+ *
+ *Return: void that means our answer is correct.
+ */
 
 int _strlen(char *s)
 {
