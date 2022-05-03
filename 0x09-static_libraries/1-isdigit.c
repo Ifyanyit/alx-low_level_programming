2900bf756bdc63fae30a76c518969aefd9f816da
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-05-02 20:25:31 -0700

    Task 0

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,16 @@
-int _isdigit(int c);
+#include "main.h"
+
+/**
+ * _isdigit - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,18 @@
-int _abs(int n);
+#include "main.h"
+
+/**
+ * _abs - check the code for Holberton School students.
+ *
+ * @n: is an integer argument
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
