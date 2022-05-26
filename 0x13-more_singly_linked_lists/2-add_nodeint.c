3023b854b8ad3cdd06d68e1ac9c9e5fea4beb8c8
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-05-26 04:54:13 -0700

    Mandatory task correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,24 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- * add_nodeint - Entry Point
- * @head: head
- * @n: value
- * Return: 0
- */
+  * add_nodeint - add new nodes to the list
+  * @head: current place in the list
+  * @n: int to add to the list
+  * Return: pointer to current position in list
+  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
+	new = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
-	return (new);
+	return (*head);
 }
