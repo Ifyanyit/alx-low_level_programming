ed7e19c6fda2c6f54d1c5ecbb4903790e34eb862
Ngene Ifeanyi <ngeneifyanyi@gmail.com>
2022-05-30 12:42:25 -0700

    Converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,30 @@
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
-{
-    int i;
-    unsigned int dec_val = 0;
+	{
+	unsigned int p;
+	int len, base;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        dec_val = 2 * dec_val + (b[i] - '0');
-    }
+	ui = 0;
 
-    return (dec_val);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base = 1; len >= 0; len--, base *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			p += base;
+		}
+	}
+
+	return (p);
 }
